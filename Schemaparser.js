90240da4351f6dd1db1f593a207b05626923b179
Fabian Abdallah <fabian.abdallah98@googlemail.com>
2020-09-20 23:07:50 +0200

    updated getallkeyfunction and added pattern function

--- Schemaparser.js
+++ Schemaparser.js
@@ -1,33 +1,9 @@
-/*var endpoints = document.getElementById('Endpoints').nodeValue.split(",");
-var read; 
-var write;
+// Endpunkte geben eine Liste von Elementen zurück
 
+//var endpoints = document.getElementById("Endpoints").nodeValue.split(",");
+//var read;
+//var write;
 
-
-function Schemaparser(endpoints,read,write){
-  this.endpoints = endpoints;
-  this.read = read;
-  this.write = write;
-  this.run = function(){
-    this.endpoints.array.forEach(endpoint => {
-      var data = fetch(endpoint).then(response => {
-        return response.json;
-      });
-      var resource = JSON.parse(data,function(key,value){
-        return key;
-      });
-      const datatypes = [];
-      for(var element in data){
-        resource = element;
-        for(var datatypes in resource)
-        datatypes
-      }
-
-    });
-  }
-  
-  }
-*/
 const aliendatabase = [
   { id: 1, name: "hi", points: 40 },
   { id: 2, name: "baum", points: 60 },
@@ -41,9 +17,93 @@ const aliendatabase = [
   { id: 10, name: "troll", points: 402434 },
   { id: 11, name: "hihihihi", points: 404234234 },
 ];
-var data = JSON.stringify({ aliens: aliendatabase });
-console.log(data);
-var resource = JSON.parse(data);
-var datatypes = Object.keys(resource);
 
-console.log(datatypes);
+var data = JSON.stringify({ aliens: aliendatabase });
+var resource = JSON.parse(data);
+
+function Schemaparser() {
+  this.read;
+  this.write;
+  this.update;
+  this.delete;
+  this.run = function run() {
+    endpoints.array.forEach((endpoint) => {
+      var data = fetch(endpoint)
+        .then((response) => {
+          return response.json;
+        })
+        .then((data) => {
+          getAllKeysFromJSON(data);
+        });
+    });
+    return true;
+  };
+  /* this.getAllKeysFromJSON = function getAllKeysFromJSON(obj) {
+    var keys = "";
+    if (typeof obj === "object" && obj !== null) {
+      keys += " { ";
+      for (key in obj) {
+        keys += " " + key + " " + getAllKeysFromJSON(obj[key]);
+      }
+      keys += " } ";
+    }
+    return keys;
+  };*/
+  this.getAllKeysFromJSON = function getAllKeysFromJSON(obj) {
+    var keys = [];
+    if (typeof obj === "object" && obj !== null) {
+      for (key in obj) {
+        keys.push(key);
+        if (typeof obj[key] === "object" && obj[key] !== null) {
+          keys.push(getAllKeysFromJSON(obj[key]));
+        }
+      }
+    }
+    return keys;
+  };
+  /*this.removeEmpty = function removeEmpty(arr) {
+    arr.forEach((element) => {
+      if (element !== []) {
+        removeEmpty(arr[element]);
+      }
+      else {
+        arr.pop()
+      }
+    });
+  };*/
+  this.findPattern = function findPattern(keys) {
+    var object = {
+      object: null,
+      counter: 0,
+    };
+    var allObjects = [];
+
+
+    keys.forEach((element) => {
+      if (Array.isArray(element)) {
+        if (
+          allObjects.find((el) => {
+            return el.object === element;
+          })
+        ) {
+          element.counter++;
+        } else {
+          var newObject = object;
+          newObject.object = element;
+          newObject.counter = 0;
+          allObjects.push(newObject);
+        }
+      }
+    });
+
+    allObjects.push(keys);
+
+    return true;
+  };
+}
+
+var schemaparser = new Schemaparser();
+console.log(schemaparser.getAllKeysFromJSON(resource));
+// console.log(keys);
+/*getAllKeysFromJSON(resource);
+console.log(allKeys);*/
